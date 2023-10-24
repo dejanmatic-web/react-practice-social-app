d6321dd1de2cf8b5927eb3ef670068ec3e1edccc
dejanwebtech <110696228+dejanwebtech@users.noreply.github.com>
2023-10-24 20:56:58 +0200

    login page design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import Login from "./pages/login/Login";
+import "./common.scss";
 function App() {
-  return <div>Hello World!</div>;
+    return (
+        <div>
+            <Login />
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Login.scss";
+const Login = () => {
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello world.</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Repellendus, nesciunt exercitationem at officiis
+                        deserunt temporibus tempora fuga dignissimos labore
+                        vero?
+                    </p>
+                    <span>Don't have account?</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form action="">
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                        />
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
